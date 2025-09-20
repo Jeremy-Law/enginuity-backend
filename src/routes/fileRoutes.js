3273791874855a407c2a3947843f845bc76145d4
Jeremy L <jeremylaw2021@gmail.com>
2025-09-20 02:51:07 -0400

    Wrong route definitions in file and projectRoutes

--- src/routes/fileRoutes.js
+++ src/routes/fileRoutes.js
@@ -14,14 +14,14 @@ router.get("/search", fileController.searchFiles);
 router.get("/recent", fileController.getRecentFiles);
 
 
-router.post("/files/:key/comments", fileController.addComment);
-router.put("/files/:key/comments/:commentId", fileController.editComment);
-router.delete("/files/:key/comments/:commentId", fileController.deleteComment);
+router.post("/:key/comments", fileController.addComment);
+router.put("/:key/comments/:commentId", fileController.editComment);
+router.delete("/:key/comments/:commentId", fileController.deleteComment);
 
 
-router.post("/files/:key/questions", fileController.addQuestion);
-router.put("/files/:key/questions/:questionId", fileController.editQuestion);
-router.delete("/files/:key/questions/:questionId", fileController.deleteQuestion);
-router.post("/files/:key/questions/:questionId/answer", fileController.answerQuestion);
+router.post("/:key/questions", fileController.addQuestion);
+router.put("/:key/questions/:questionId", fileController.editQuestion);
+router.delete("/:key/questions/:questionId", fileController.deleteQuestion);
+router.post("/:key/questions/:questionId/answer", fileController.answerQuestion);
 
 module.exports = router;
--- src/routes/projectsRoutes.js
+++ src/routes/projectsRoutes.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const projectController = require('../controllers/projectController');
 
 
-router.get('/projects', projectController.getAllProjects);
-router.post('/projects', projectController.createProject);
-router.get('/projects/:id', projectController.getProject);
-router.put('/projects/:id', projectController.editProject);
-router.delete('/projects/:id', projectController.deleteProject);
+router.get('/', projectController.getAllProjects);
+router.post('/', projectController.createProject);
+router.get('/:id', projectController.getProject);
+router.put('/:id', projectController.editProject);
+router.delete('/:id', projectController.deleteProject);
 
 
-router.post('/projects/:id/users', projectController.addUserToProject);
-router.delete('/projects/:id/users/:userId', projectController.removeUserFromProject);
+router.post('/:id/users', projectController.addUserToProject);
+router.delete('/:id/users/:userId', projectController.removeUserFromProject);
 
 module.exports = router;
